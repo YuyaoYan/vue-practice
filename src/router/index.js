5069061ca347acb1e8335247ba62a3d96e939d70
YuyaoYan <yanyy2@yonyou.com>
2020-04-19 23:01:19 +0800

    add asyncRouter

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,48 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
-Vue.use(VueRouter)
-
-  const routes = [
+import Vue from "vue";
+import Router from "vue-router";
+import Layout from "@/layout"; // 布局页
+Vue.use(Router);
+// 通用页面
+export const constRoutes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/login",
+    component: () => import("@/views/Login"),
+    hidden: true, // 导航菜单忽略该项
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
-  routes
-})
-
-export default router
+    path: "/",
+    component: Layout, // 应用布局
+    redirect: "/home",
+    children: [
+      {
+        path: "home",
+        component: () => import(/* webpackChunkName: "home" */ "@/views/Home.vue"),
+        name: "home",
+        meta: {
+          title: "Home", // 导航菜单项标题
+          icon: "qq", // 导航菜单项图标 }
+        },
+      },
+    ],
+  },
+];
+export const asyncRoutes = [
+  {
+    path: "/about",
+    component: Layout,
+    redirect: "/about/index",
+    children: [
+      {
+        path: "index",
+        component: () => import(/* webpackChunkName: "home" */ "@/views/About.vue"),
+        name: "about",
+        meta: {
+          title: "About",
+          icon: "qq",
+          roles: ["admin", "editor"],
+        },
+      },
+    ],
+  },
+];
+export default new Router({ mode: "history", base: process.env.BASE_URL, routes: constRoutes });
